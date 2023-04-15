c6bd707675a66495674c97c3926e6dd1bbfa754b
tsavaph <tsavaph@gmail.com>
2023-04-15 16:11:56 +0300

    completed

--- src/main/java/mobi/sevenwinds/app/budget/BudgetApi.kt
+++ src/main/java/mobi/sevenwinds/app/budget/BudgetApi.kt
@@ -1,5 +1,6 @@
 package mobi.sevenwinds.app.budget
 
+import com.fasterxml.jackson.annotation.JsonInclude
 import com.papsign.ktor.openapigen.annotations.parameters.PathParam
 import com.papsign.ktor.openapigen.annotations.parameters.QueryParam
 import com.papsign.ktor.openapigen.annotations.type.number.integer.max.Max
@@ -33,33 +34,50 @@ fun NormalOpenAPIRoute.budget() {
         }
     }
 }
-
 data class BudgetRecord(
     @Min(1900) val year: Int,
     @Min(1) @Max(12) val month: Int,
     @Min(1) val amount: Int,
     val type: BudgetType,
-    val authorId: Int?,
+    @JsonInclude(JsonInclude.Include.NON_NULL) val authorId: Int?,
 ) {
     constructor(year: Int, month: Int, amount: Int, type: BudgetType)
             : this(year, month, amount, type, null)
 }
 
+data class BudgetRecordWithAuthorNameAndDate(
+    val year: Int,
+    val month: Int,
+    val amount: Int,
+    val type: BudgetType,
+    @JsonInclude(JsonInclude.Include.NON_NULL) val authorName: String?,
+
+    @JsonInclude(JsonInclude.Include.NON_NULL) val authorCreatedAt: String?,
+) {
+    constructor(budgetRecord: BudgetRecord)
+            : this(budgetRecord.year, budgetRecord.month, budgetRecord.amount, budgetRecord.type, null, null)
+    constructor(budgetRecord: BudgetRecord, authorName: String?, authorCreatedAtString: String?)
+            : this(budgetRecord.year, budgetRecord.month, budgetRecord.amount, budgetRecord.type, authorName, authorCreatedAtString)
+}
 data class BudgetYearParam(
     @PathParam("Год") val year: Int,
     @QueryParam("Лимит пагинации") val limit: Int,
     @QueryParam("Смещение пагинации") val offset: Int,
+
+    @QueryParam("ФИО автора")
+    @MinLength(2)
+    val authorName: String?,
 )
 
 class BudgetYearStatsResponse(
     val total: Int,
     val totalByType: Map<String, Int>,
-    val items: List<BudgetRecord>
+    val items: List<BudgetRecordWithAuthorNameAndDate>,
 )
 
 
 data class AuthorRecord(
-    @MinLength(1) val authorName: String
+    @MinLength(2) val authorName: String
 )
 
 enum class BudgetType {
--- src/main/java/mobi/sevenwinds/app/budget/BudgetService.kt
+++ src/main/java/mobi/sevenwinds/app/budget/BudgetService.kt
@@ -2,9 +2,14 @@ package mobi.sevenwinds.app.budget
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import mobi.sevenwinds.app.budget.author.AuthorEntity
+import mobi.sevenwinds.app.budget.author.AuthorTable
+import org.jetbrains.exposed.sql.Query
 import org.jetbrains.exposed.sql.SortOrder
 import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.transactions.transaction
+import org.joda.time.format.DateTimeFormat
+import org.joda.time.format.DateTimeFormatter
 
 object BudgetService {
     suspend fun addRecord(body: BudgetRecord): BudgetRecord = withContext(Dispatchers.IO) {
@@ -23,22 +28,54 @@ object BudgetService {
 
     suspend fun getYearStats(param: BudgetYearParam): BudgetYearStatsResponse = withContext(Dispatchers.IO) {
         transaction {
+            val authorName = param.authorName
+
             val query = BudgetTable.select { BudgetTable.year eq param.year }
-            val data = BudgetEntity.wrapRows(query).map { it.toResponse() }
-            val total = query.count()
+            val data = getFilteredData(query, authorName)
+            val total = data.count()
 
             val limitedQuery = query.limit(param.limit, param.offset)
                 .orderBy(BudgetTable.month to SortOrder.ASC)
                 .orderBy(BudgetTable.amount to SortOrder.DESC)
 
-            val limitedData = BudgetEntity.wrapRows(limitedQuery).map { it.toResponse() }
+            val limitedData = getFilteredData(limitedQuery, authorName)
             val sumByType = data.groupBy { it.type.name }.mapValues { it.value.sumOf { v -> v.amount } }
 
             return@transaction BudgetYearStatsResponse(
                 total = total,
                 totalByType = sumByType,
-                items = limitedData
+                items = limitedData,
             )
+        }
+    }
+
+    private fun getFilteredData(query: Query, authorName: String?) : List<BudgetRecordWithAuthorNameAndDate> {
+
+        val data = BudgetEntity.wrapRows(query).map { it.toResponse() }.map { budgetRecord ->
+            val authorId =  budgetRecord.authorId
+
+            if (authorId == null) {
+                return@map BudgetRecordWithAuthorNameAndDate(budgetRecord)
+            } else {
+                val authorQuery = AuthorTable.select { AuthorTable.id eq authorId }
+                val authorEntity = AuthorEntity.wrapRow(authorQuery.first())
+
+                val fullAuthorName = authorEntity.authorName
+                val authorCreatedAt = authorEntity.createdAt
+
+                val dtf: DateTimeFormatter = DateTimeFormat.forPattern("dd.MM.yyyy HH:mm:ss")
+                val authorCreatedAtString = dtf.print(authorCreatedAt)
+
+                return@map BudgetRecordWithAuthorNameAndDate(budgetRecord, fullAuthorName, authorCreatedAtString)
+            }
+        }
+
+        return if (authorName == null) {
+            data
+        } else {
+            data.filter { budgetRecordWithAuthorNameAndDate ->
+                return@filter budgetRecordWithAuthorNameAndDate.authorName?.contains(authorName, ignoreCase = true) == true
+            }
         }
     }
 }
